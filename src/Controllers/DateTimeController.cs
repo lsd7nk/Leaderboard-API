cf96472e56e38e08fabd1de8756326ec91eca8e7
lsd7nk <87329383+lsd7nk@users.noreply.github.com>
2024-06-11 00:52:25 +0700

    add DateTimeController.cs

---
+++ src/Controllers/DateTimeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Leaderboard.Services;
+
+namespace Leaderboard.Controllers
+{
+    [Route("leaderboard")]
+    [ApiController]
+    public sealed class DateTimeController : ControllerBase
+    {
+        private readonly DateTimeService _dateTimeService;
+
+        public DateTimeController(DateTimeService dateTimeService)
+        {
+            _dateTimeService = dateTimeService;
+        }
+
+        [HttpGet("datetime")]
+        public async Task<string> Get()
+        {
+            return await new Task<string>(_dateTimeService.GetDateTime);
+        }
+    }
+}
